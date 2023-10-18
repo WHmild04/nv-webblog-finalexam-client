7502c18cdfb61c7d751ee667681c085fbf86c620
WHmild04 <112480328+WHmild04@users.noreply.github.com>
2023-10-18 14:17:59 +0700

    stert

---
+++ src/Course/course.js
@@ -0,0 +1,8 @@
+{
+    "title": "Server Side Programming",
+    "instructor": "Dr. Nattapon Kumyaito",
+    "description": "Client-server web architecture, overview of server-side programming languages, server-side programming basics including types, arrays, loops, functions, handling form input and validation; database connections and making database queries, object-oriented approaches to server-side programming, designing and writing web services, authentication techniques and session management, use of server-side frameworks, web organization and standards",
+    "total_student": 50,
+    "status": "active",
+    "type": "core subject"
+}
